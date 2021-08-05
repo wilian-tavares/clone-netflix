d95b370ba8c17e8bb13c67027599008d4657c518
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-04 23:48:10 -0300

    Primeiro Envio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+let novaPagina = window.open('https://www.youtube.com/watch?v=3GoDZUf_SPQ&ab_channel=F%C3%A1bioRabin')
+
+novaPagina = document.getElementById('novoFilme').novaPagina;
